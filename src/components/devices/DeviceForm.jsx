ed47e0ce5723a7a6de79101d4203c5e3a1cbb4bf
Wahbi Baklouti <wahbibaklouti7@gmail.com>
2025-02-03 09:35:36 +0100

    fix:Update device form

--- src/components/devices/DeviceForm.jsx
+++ src/components/devices/DeviceForm.jsx
@@ -1,49 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Button, Checkbox, Form, Input, message } from 'antd';
-import { createDevice, updateDevice } from '../../services/DevicesService';
 import { useDevices } from '../../context/DevicesContext';
 
 const DeviceForm = ({ device, setDevices, setIsModalOpen }) => {
 
-    // const [form] = Form.useForm();
-    const {handleAddDevice,handleUpdateDevice,form } = useDevices()
+  const { handleAddDevice, handleUpdateDevice, form } = useDevices()
+  
+  useEffect(() => {
+    if (device) {
+        form.setFieldsValue({
+            name: device.name
+        })
+    }
+  }, [device])
     const onFinish = async (values) => {
         if (!device) {
-            // createDevice(values).then((res) => {
-            //     if (res.status) {
-            //       // setDevices((prev) => [...prev, res.data]);
-            //         addDevice(res.data)
-            //         setIsModalOpen(false);
-            //     } else {
-            //         form.setFields([
-            //             {
-            //                 name: 'name',
-            //                 errors: [res.message],
-            //             },
-            //         ])
-            //     }
-               
-            // });
+
           await handleAddDevice(values)
           setIsModalOpen(false)
           message.success('Device added successfully!');
           
         } else {
-            // updateDevice(device.id, values).then((res) => {
-                
-            //     if (res.status) {
-            //         setDevices((prev) => prev.map((d) => (d.id === res.data.id ? res.data : d)));
-            //     setIsModalOpen(false);
-            //     } else {
-            //         form.setFields([
-            //             {
-            //                 name: 'name',
-            //                 errors: [res.message],
-            //             },
-            //         ])
-            //     }
-                
-            // });
+
           handleUpdateDevice(device.id,values,() => setIsModalOpen(false))
           
         }
@@ -65,7 +43,6 @@ const DeviceForm = ({ device, setDevices, setIsModalOpen }) => {
       maxWidth: 600,
     }}
     initialValues={{
-        remember: true,
         name: device?.name
     }}
     onFinish={onFinish}
